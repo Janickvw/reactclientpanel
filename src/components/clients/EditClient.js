9038d2a9d7bca4b1d2b536d8ad6b7d5420428207
Janickvw <janick.van.well@gmail.com>
2018-09-25 13:50:06 +1200

    Finished edit client

--- src/components/clients/EditClient.js
+++ src/components/clients/EditClient.js
@@ -7,12 +7,131 @@ import { firestoreConnect } from 'react-redux-firebase';
 import Spinner from '../layout/Spinner';
 
 class EditClient extends Component {
+  constructor(props) {
+    super(props);
+
+    this.firstNameInput = React.createRef();
+    this.lastNameInput = React.createRef();
+    this.emailInput = React.createRef();
+    this.phoneInput = React.createRef();
+    this.balanceInput = React.createRef();
+  }
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const { client, firestore, history } = this.props;
+
+    // Update Client
+    const updClient = {
+      firstName: this.firstNameInput.current.value,
+      lastName: this.lastNameInput.current.value,
+      email: this.emailInput.current.value,
+      phone: this.phoneInput.current.value,
+      balance:
+        this.balanceInput.current.value === ''
+          ? 0
+          : this.balanceInput.current.value
+    };
+
+    // Update client in firestore
+    firestore
+      .update({ collection: 'clients', doc: client.id }, updClient)
+      .then(history.push('/'));
+  };
+
   render() {
     const { client } = this.props;
     if (client) {
-      return <div>{client.firstName}</div>;
+      return (
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+              <Link to="/" className="btn btn-link float-left">
+                <i className="fas fa-arrow-circle-left" /> Back To Dashboard
+              </Link>
+            </div>
+          </div>
+
+          <div className="card">
+            <div className="card-header">Add Client</div>
+            <div className="card-body">
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <label htmlFor="firstName">First Name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="firstName"
+                    minLength="2"
+                    required
+                    onChange={this.onChange}
+                    ref={this.firstNameInput}
+                    defaultValue={client.firstName}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="lastName">Last Name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="lastName"
+                    minLength="2"
+                    required
+                    onChange={this.onChange}
+                    ref={this.lastNameInput}
+                    defaultValue={client.lastName}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    name="email"
+                    minLength="2"
+                    required
+                    onChange={this.onChange}
+                    ref={this.emailInput}
+                    defaultValue={client.email}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="phone">Phone</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="phone"
+                    minLength="9"
+                    required
+                    onChange={this.onChange}
+                    ref={this.phoneInput}
+                    defaultValue={client.phone}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="balance">Balance</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="balance"
+                    onChange={this.onChange}
+                    ref={this.balanceInput}
+                    defaultValue={client.balance}
+                  />
+                </div>
+                <input
+                  type="submit"
+                  value="Submit"
+                  className="btn btn-primary btn-block"
+                />
+              </form>
+            </div>
+          </div>
+        </div>
+      );
     } else {
-      return;
+      return <Spinner />;
     }
   }
 }
